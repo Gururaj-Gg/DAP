c3624d75bb53d79bfa81c5c0ed80e95e018c202d
GURURAJ G <79778518+Gururaj-Gg@users.noreply.github.com>
2022-12-06 15:19:24 +0530

    Delete b2py2.py

--- b2py2.py
+++
@@ -1,9 +0,0 @@
-list1=[]
-n=int(input("enter the size of element list"))
-for i in range(0,n):
-        e=input("enter the element")
-        list1.append(e)
-        list1=sorted(list1)
-print("the sorted elements are:",list1)
-
-
